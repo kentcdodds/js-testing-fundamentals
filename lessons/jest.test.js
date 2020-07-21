70efd862ee15957979d10b139fe0d2a936018a31
MujunZ <o.6180339@gmail.com>
2020-07-21 15:28:00 -0700

    add exercise for jest.test

--- lessons/jest.test.js
+++ lessons/jest.test.js
@@ -1,3 +1,17 @@
+/**
+ * Up to this point we’ve created all our own utilities.
+ * As it turns out, the utilities we’ve created mirror the
+ * utilities provided by Jest perfectly! Let’s install Jest
+ * and use it to run our test!
+ *
+ * The testing framework we've written actually looks
+ * remarkably a lot like Jest. Rather than running `node --require`,
+ * and then instead of globals and so on and so forth, we could
+ * actually just use Jest directly.
+ *
+ * Task: run `npx jest`
+ */
+
 const {sumAsync, subtractAsync} = require('../math')
 
 test('sumAsync adds numbers asynchronously', async () => {
