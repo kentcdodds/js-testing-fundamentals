42588ed084e519a05a2c32067f6c4dde57c9ed66
MujunZ <o.6180339@gmail.com>
2020-07-21 15:20:12 -0700

    add exercise for globals

--- lessons/globals.js
+++ lessons/globals.js
@@ -1,3 +1,20 @@
+/**
+ * These testing utilities that we built are pretty useful.
+ * We want to be able to use them throughout our application
+ * in every single one of our test files.
+ *
+ * Some testing frameworks provide their helpers as global
+ * variables. Let’s implement this functionality to make it
+ * easier to use our testing framework and assertion library.
+ * We can do this by exposing our test and expect functions on
+ * the global object available throughout the application.
+ *
+ * Task: Create global object in "setup-globals.js" so that we can use `test` and
+ * `expect` in any file inside the project.
+ *
+ * Execute: Use `node --require ./setup-globals.js lessons/globals.js` to run the test.
+ */
+
 const {sumAsync, subtractAsync} = require('../math')
 
 test('sumAsync adds numbers asynchronously', async () => {
@@ -11,3 +28,9 @@ test('subtractAsync subtracts numbers asynchronously', async () => {
   const expected = 4
   expect(result).toBe(expected)
 })
+
+/**
+ * Hint: https://nodejs.org/api/globals.html#globals_global
+ *
+ * Answer: Checkout the master branch for the answer.
+ */
