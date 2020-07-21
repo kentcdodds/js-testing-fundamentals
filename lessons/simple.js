d8d13922f17f29e7b17ffebca1f2bec2a56c682d
MujunZ <o.6180339@gmail.com>
2020-07-21 11:51:18 -0700

    add exercise for simple

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,22 @@
+/**
+ * We have two functions, `sum` and `subtract` ,
+ * that are ready to be tested.
+ *
+ * Task: check if the results are the respects.
+ *
+ *            **BONUS**
+ * Throw an error if the test fails.
+ *
+ *
+ * Execute: Use `node lessons/simple.js` to run the test.
+ */
+
 const {sum, subtract} = require('../math')
 
 let result, expected
 
-result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
-
-result = subtract(7, 3)
-expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+/**
+ * Hint: there's no magic, just the most straightforward conditional statement.
+ *
+ * Answer: Checkout the master branch for the answer.
+ */
