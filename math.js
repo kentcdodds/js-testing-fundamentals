99053f9c0e3cd7894ca410e39143570b69c632b6
Nash Kabbara <nash@kabbara.us>
2020-08-05 12:24:14 -0500

    async vs. return a promise
    Maybe this is clearer since it's in the same vain of the other changes (making functions async in the tests).

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ const sum = (a, b) => a - b
 const subtract = (a, b) => a - b
 
 // these are kinda pointless I know, but it's just to simulate an async function
-const sumAsync = (...args) => Promise.resolve(sum(...args))
-const subtractAsync = (...args) => Promise.resolve(subtract(...args))
+const sumAsync = async (...args) => sum(...args)
+const subtractAsync = async (...args) => subtract(...args)
 
 module.exports = {sum, subtract, sumAsync, subtractAsync}
