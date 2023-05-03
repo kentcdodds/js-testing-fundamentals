39e4d47df3716b03e2c9c1eb61ddfa342ebfdcd9
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-03 10:39:34 -0500

    Fixed simple math based on the simple test script

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,16 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require('../math');
 
-let result, expected
+let result, expected;
 
-result = sum(3, 7)
-expected = 10
+//This is the simple way to run tests in JS
+result = sum(3, 7);
+expected = 10;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 3)
-expected = 4
+result = subtract(7, 3);
+expected = 4;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
--- math.js
+++ math.js
@@ -1,9 +1,9 @@
 // sum is intentionally broken so you can see errors in the tests
-const sum = (a, b) => a - b
-const subtract = (a, b) => a - b
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 
 // these are kinda pointless I know, but it's just to simulate an async function
-const sumAsync = (...args) => Promise.resolve(sum(...args))
-const subtractAsync = (...args) => Promise.resolve(subtract(...args))
+const sumAsync = (...args) => Promise.resolve(sum(...args));
+const subtractAsync = (...args) => Promise.resolve(subtract(...args));
 
-module.exports = {sum, subtract, sumAsync, subtractAsync}
+module.exports = { sum, subtract, sumAsync, subtractAsync };
