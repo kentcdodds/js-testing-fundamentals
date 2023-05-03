b79d9d8492ca0da8c1f505327c3424778c426961
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-03 10:55:06 -0500

    Played around with own assertion test, greater than. Still finiding application of test format :P

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -1,21 +1,31 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract, difference } = require('../math');
 
-let result, expected
+let result, expected;
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+result = sum(3, 7);
+expected = 10;
+expect(result).toBe(expected);
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+result = subtract(7, 3);
+expected = 4;
+expect(result).toBe(expected);
+
+result = difference(3, 7);
+expected = 4;
+expect(result).toBeGreaterThan(expected);
+console.log(difference(3, 7));
 
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
-    }
-  }
+    },
+    toBeGreaterThan(expected) {
+      if (actual < expected) {
+        throw new Error(`${actual} is not greater than ${expected}`);
+      }
+    },
+  };
 }
