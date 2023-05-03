a4cdfc7fe99a55ef5554ad8d9f37086461f3e083
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-03 12:07:44 -0500

    Jest test written for difference

--- lessons/jest.test.js
+++ lessons/jest.test.js
@@ -1,13 +1,19 @@
-const {sumAsync, subtractAsync} = require('../math')
+const { sumAsync, subtractAsync, differenceAsync } = require('../math');
 
 test('sumAsync adds numbers asynchronously', async () => {
-  const result = await sumAsync(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
 test('subtractAsync subtracts numbers asynchronously', async () => {
-  const result = await subtractAsync(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+test('difference as Async', async () => {
+  const result = await differenceAsync(2, 9);
+  const expected = 7;
+  expect(result).toBe(expected);
+});
