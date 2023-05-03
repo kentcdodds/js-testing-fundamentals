bba7bfcfb4d73f26132db792c35b1b04950c1570
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-03 11:35:02 -0500

    Testing framework, added test for difference

--- lessons/testing-framework.js
+++ lessons/testing-framework.js
@@ -1,24 +1,30 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract, difference } = require('../math');
 
 test('sum adds numbers', () => {
-  const result = sum(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
 test('subtract subtracts numbers', () => {
-  const result = subtract(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+test('difference finds the absolute difference', () => {
+  const result = difference(2, 7);
+  const expected = 5;
+  expect(result).toBe(expected);
+});
 
 function test(title, callback) {
   try {
-    callback()
-    console.log(`✓ ${title}`)
+    callback();
+    console.log(`✓ ${title}`);
   } catch (error) {
-    console.error(`✕ ${title}`)
-    console.error(error)
+    console.error(`✕ ${title}`);
+    console.error(error);
   }
 }
 
@@ -26,8 +32,8 @@ function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
-    }
-  }
+    },
+  };
 }
