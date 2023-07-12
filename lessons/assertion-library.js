e624b01cb62e7f4c6dc4fbc96f8e30b42532b93d
Oliver Day <oliver.day.swe@gmail.com>
2023-07-11 17:32:36 -0700

    feat: update assertion library expect function

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -1,5 +1,17 @@
 const {sum, subtract} = require('../math')
 
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    // toEqual(expected) {},
+		// toBeGreaterThan(expected) {},
+  }
+}
+
 let result, expected
 
 result = sum(3, 7)
@@ -9,13 +21,3 @@ expect(result).toBe(expected)
 result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
-    }
-  }
-}
