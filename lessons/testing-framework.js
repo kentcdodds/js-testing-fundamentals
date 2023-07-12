89c41f9cb41456bc426f5dff8b6133ba63a014c3
Oliver Day <oliver.day.swe@gmail.com>
2023-07-11 17:42:01 -0700

    feat: testing-framework code organization

--- lessons/testing-framework.js
+++ lessons/testing-framework.js
@@ -1,16 +1,4 @@
-const {sum, subtract} = require('../math')
-
-test('sum adds numbers', () => {
-  const result = sum(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
-
-test('subtract subtracts numbers', () => {
-  const result = subtract(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+const { sum, subtract } = require('../math')
 
 function test(title, callback) {
   try {
@@ -31,3 +19,15 @@ function expect(actual) {
     }
   }
 }
+
+test('sum adds numbers', () => {
+  const result = sum(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
+
+test('subtract subtracts numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
